ebcabe7bf1a669c9ddd36d1deb9b2b88cb415342
Leslie Torres <lesliejailine@hotmail.com>
2024-06-13 17:14:26 -0500

    first commit

---
+++ components/products/ProductsPagination.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+type ProductsPaginationProps = {
+    page: number
+    totalPages: number
+}
+
+export default function ProductsPagination({page, totalPages} : ProductsPaginationProps) {
+  const pages = Array.from({length:8}, (_,i) => i+1 )
+
+  return (
+    <nav className="flex justify-center py-16">
+        {page > 1 && (
+        <Link className="bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0" href={`/admin/products?page=${page - 1}`}>&laquo;</Link>
+      )}
+      {pages.map(currentPage => (
+        <Link  key={currentPage} className={`${page === currentPage ? 'font-black': 'bg-white' } px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0`} href={`/admin/products?page=${currentPage}`} >{currentPage}</Link>
+      ))}
+      {page < totalPages && (
+        <Link className="bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0" href={`/admin/products?page=${page + 1}`}>&raquo;</Link>
+      )}
+    </nav>
+  );
+}
